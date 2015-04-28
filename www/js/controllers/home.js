1f9e32dce69a0aefd815f7a006b648e58a0bbe12
Pradeepraj Kunrathur Chandrasekaran <prashanth_cvvp@yahoo.co.in>
2015-04-28 01:28:07 -0500

    28th April 2015

--- www/js/controllers/home.js
+++ www/js/controllers/home.js
@@ -4,7 +4,7 @@
 define([
 	'mkn'
 	], function (app) {
-	return app.controller('HomeController', function ($scope, $location,$modal, $log) {
+	return app.controller('HomeController', function ($scope, $location,$modal, $log,$http) {
 		//$scope.homeMessage = "Home Message Home Message Home Message Home Message Home Message Home Message Home Message Home Message Home Message Home Message Home Message Home Message Home Message Home Message Home Message Home Message Home Message Home Message Home Message Home Message Home Message Home Message ";
 
   $scope.myInterval = 5000;
@@ -20,24 +20,11 @@ define([
 	}
   ];
   
-  $scope.values = [	
-	{
-		path:'../../images/fruits2.jpg',
-		caption:'Fruits2'
-	},
-    {
-		path:'../../images/fruits4.jpg',
-		caption:"Fruits4"
-	},
-	{
-		path:'../../images/veggies2.jpg',
-		caption:"Vegetables"
-	},
-	{
-		path:'../../images/fruits4.jpg',
-		caption:"Fruits"
-	}
-	];
+  $http.get('../../dbfiles/images.txt')
+  	.success(function(response)
+  		{
+  			$scope.values=response.images;
+  		});
 	
 
   //login modal
